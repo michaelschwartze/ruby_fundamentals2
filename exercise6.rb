97b634d3bd185b253bf1c16b2277d447bfc234e5
Michael Schwartze <mschwartze@gmail.com>
2016-10-19 14:07:05 -0400

    Add file with grocery list and code to print out list items with asterix

---
+++ exercise6.rb
@@ -0,0 +1,9 @@
+#Grocery list exercise
+
+grocery_list = ["lemons", "limes", "milk", "cheese"]
+
+#Present list on each line preceded by an asterix
+
+grocery_list.each do |item|
+  puts "* " + item
+end
