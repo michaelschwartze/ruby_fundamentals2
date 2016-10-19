a9060862e8c6d5ff5dd2eea543faf75d52a35940
Michael Schwartze <mschwartze@gmail.com>
2016-10-19 13:49:15 -0400

    Added comments to exercises file that explains program

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,13 @@
+#Asks user for a temperature in Farenheight, converts to Celcius and returns the converted temperature in a sentence
+
+#Inputs:
 puts "Enter a temperature in Farenheight:"
 user_temp = gets.chomp
 
+#Method:
 def f_to_c(f_temp)
   return (f_temp.to_i - 32) * 5 / 9
 end
+
+#Outputs:
+puts f_to_c(user_temp)
